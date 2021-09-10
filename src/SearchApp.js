e0f491ffe15a3dd25c01bb834512cde77da49c44
fcotabar <fcotabar@gmail.com>
2021-09-09 21:56:48 -0500

    Project initilize with mock db

---
+++ src/SearchApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SearchApp = () => {
+  return (
+    <div>
+      <h1>I'm the searching bar</h1>
+    </div>
+  );
+};
---
+++ src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SearchBox = () => {
+  return (
+    <div>
+      <h1>here is the search box</h1>
+    </div>
+  );
+};
