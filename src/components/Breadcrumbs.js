bcf3b4003e7c2613408d6977ee384883210176fc
fcotabar <fcotabar@gmail.com>
2021-09-12 17:46:43 -0500

    Breadcrumbs full responsive with grid

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -4,16 +4,18 @@ export const Breadcrumbs = ({ categories }) => {
   if (categories.length === 0) return '';
 
   return (
-    <ol className="breadcrumbs">
-      <li className="breadcrumbs__item-title">Búsquedas relacionadas:</li>
-      {categories.map((cat, i, arr) => (
-        <li key={i} className="breadcrumbs__item">
-          {cat.trim()}{' '}
-          {arr.length !== i + 1 && (
-            <span className="breadcrumbs__item-division">|</span>
-          )}
-        </li>
-      ))}
-    </ol>
+    <nav className="breadcrumbs">
+      <div className="breadcrumbs__item-title">Búsquedas relacionadas:</div>
+      <ul className="breadcrumbs__item-list">
+        {categories.map((cat, i, arr) => (
+          <li key={i} className="breadcrumbs__item">
+            {cat.trim()}{' '}
+            {arr.length !== i + 1 && (
+              <span className="breadcrumbs__item-division">|</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
